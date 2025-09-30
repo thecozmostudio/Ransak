c6491ef4d7be7f6448e54318d4a5d17e1b32ff7a
Kavin <kavinmadusanka20011@gmail.com>
2025-09-30 17:07:07 +0530

    home screen image changing part done

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import Layout from '../Layout/Layout';
 import { useTheme } from '../Context/ThemeProvider';
+import Carousel from '../Components/Carousel';
 
 const Home = () => {
   const {activeTheme, mode } = useTheme();
 
   return (
     <Layout>
-      <div className='p-6'>
-        <h1
-            style={{
-              color: activeTheme.colors.primary,
-            }}
-            >
+      <div className="relative w-full h-screen ">
+        {/* Carousel in background */}
+        <Carousel />
+
+        {/* Overlay content */}
+        {/* <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+          <h1
+            className="text-4xl md:text-6xl font-bold drop-shadow-lg"
+            style={{ color: activeTheme.colors.primary }}
+          >
             Packaging Company ({mode} mode)
           </h1>
 
-          <p 
-          style={{ color: activeTheme.colors.secondary }}
+          <p
+            className="mt-4 text-lg md:text-2xl drop-shadow-lg"
+            style={{ color: activeTheme.colors.secondary }}
           >
             We design and deliver custom packaging.
           </p>
+        </div> */}
       </div>
     </Layout>
   )
--- Client/src/Pages/PageNotFound.jsx
+++ Client/src/Pages/PageNotFound.jsx
@@ -4,7 +4,7 @@ import Layout from './../Layout/Layout';
 const PageNotFound = () => {
   return (
     <Layout>
-      <div className="h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+      <div className="h-screen flex items-center justify-center">
         <h1 className="text-3xl font-bold text-red-600 dark:text-red-400">
           404 - Page Not Found
         </h1>
